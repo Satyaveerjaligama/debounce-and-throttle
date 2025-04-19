f2a6d5681207a08b3b2ff49e2a9fc2d0fde45119
Satyaveer Jaligama <satyaveerjaligama13@gmail.com>
2025-04-20 00:45:14 +0530

    Added code for Debouncing and Throttling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import _ from "lodash";
 
 function App() {
+  const getSuggestions = (e: any) => {
+    console.log("API call", e);
+  };
+
+  const handleChange = _.debounce(getSuggestions, 3000, {
+    // leading: true,
+    trailing: true,
+    maxWait: 2000,
+  });
+
+  const cancelDebounce = () => {
+    console.log("Cancel Debounce");
+    handleChange.cancel();
+  };
+
+  const flushDebounce = () => {
+    console.log("Flush Debounce");
+    handleChange.flush();
+  };
+
+  const handleClick = () => {
+    console.log("Button click");
+  };
+
+  const handleThrottle = _.throttle(handleClick, 4000, {
+    // leading: false,
+    // trailing: true,
+  });
+
+  const cancelThrottle = () => {
+    console.log("Cancel Throttle");
+    handleThrottle.cancel();
+  };
+
+  const flushThrottle = () => {
+    console.log("Flush Throttle");
+    handleThrottle.flush();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input placeholder="Start Typing..." onChange={(e) => handleChange(e)} />
+      <button onClick={cancelDebounce}>Cancel Debounce</button>
+      <button onClick={flushDebounce}>Flush Debounce</button>
+      <br></br>
+      <br></br>
+      <button onClick={handleThrottle}>Throttle</button>
+      <button onClick={cancelThrottle}>Cancel Throttle</button>
+      <button onClick={flushThrottle}>Flush Throttle</button>
     </div>
   );
 }
